49c97c97cabb6f4c196c66f6485a713facdd8bf4
Keshawn Triplett <keshawntriplett@keshawns-mbp>
2017-03-22 12:50:06 -0500

    Courses Table View Controller addition

--- Courses.swift
+++ Courses.swift
@@ -8,21 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class Courses: UIViewController,UITableViewDelegate {
     
-    
-    
+    @IBOutlet weak var CoursesTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
